20f85f2beb546f6b5ba898672bb44aa6c83560a4
AMEYA <AMEYA@PRIVATE-DESKTOP>
2022-07-07 14:26:08 +0530

    finally

---
+++ WORLD/src/INDIA/MH.java
@@ -0,0 +1,23 @@
+package INDIA;
+
+public class MH {
+
+	//ADDITION OF TWO NUMBERS
+	
+	void add ()
+	{
+		int a=10,b=20;
+		int sum=a+b;
+	System.out.println(sum);}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MH a = new MH ()  ;
+		a.add();
+		
+		
+	}
+
+}
